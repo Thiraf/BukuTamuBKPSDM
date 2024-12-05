caa502b469c77e454497430ed63942f666f4aac4
Ananda Hirzi Thirafi <119308548+Thiraf@users.noreply.github.com>
2024-12-05 07:44:09 +0700

    update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,15 +1,13 @@
-#!/bin/bash
 
-# Cek apakah migrasi sudah dijalankan sebelumnya dengan melihat apakah file penanda ada
 if [ ! -f /var/www/storage/.migrations_ran ]; then
     echo "Menjalankan migrasi dan seeder..."
     php artisan migrate --force
     php artisan db:seed --force
-    # Tandai bahwa migrasi sudah dijalankan dengan membuat file penanda
+
     touch /var/www/storage/.migrations_ran
 else
     echo "Migrasi dan seeder sudah dijalankan sebelumnya."
 fi
 
-# Setelah migrasi, jalankan PHP-FPM
+
 exec php-fpm
